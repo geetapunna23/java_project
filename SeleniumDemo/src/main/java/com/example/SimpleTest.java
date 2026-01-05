5c15e079c6c26846e15bb52c075d1f9cbe2e069b
geetapunna23 <geetapunna23@gmail.com>
2026-01-05 16:15:18 +0000

    intial running test

--- SeleniumDemo/src/main/java/com/example/SimpleTest.java
+++ SeleniumDemo/src/main/java/com/example/SimpleTest.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class SimpleTest {
     public static void main(String[] args) {
@@ -12,7 +17,12 @@ public class SimpleTest {
         WebDriver driver = new ChromeDriver();
 
         // Open a website
-        driver.get("https://www.google.com");
+        driver.get("https://www.itv.com");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement element = wait.until(
+        ExpectedConditions.visibilityOfElementLocated(By.cssSelector("cassie-pre-banner--button--container"))
+);
 
         // Print title
         System.out.println("Page title is: " + driver.getTitle());
